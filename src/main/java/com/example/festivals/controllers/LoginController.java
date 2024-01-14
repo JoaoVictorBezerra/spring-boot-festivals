5f75794d8846e3dc2ecb006cccac4ee8b63e2d18
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 19:45:57 -0300

    refactor: changed LoginService to AuthenticationService

--- src/main/java/com/example/festivals/controllers/LoginController.java
+++ src/main/java/com/example/festivals/controllers/LoginController.java
@@ -1,7 +1,7 @@
 package com.example.festivals.controllers;
 
 import com.example.festivals.dto.request.LoginRequestDTO;
-import com.example.festivals.services.LoginService;
+import com.example.festivals.services.AuthorizationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/login")
 public class LoginController {
 
-  private final LoginService loginService;
-  public LoginController(LoginService loginService) {
-    this.loginService = loginService;
+  private final AuthorizationService authorizationService;
+  public LoginController(AuthorizationService authorizationService) {
+    this.authorizationService = authorizationService;
   }
 
   @PostMapping
   public ResponseEntity execute(@RequestBody LoginRequestDTO user) {
     try {
-      return ResponseEntity.status(HttpStatus.OK).body(loginService.execute(user.email(), user.password()));
+      return ResponseEntity.status(HttpStatus.OK).body(authorizationService.login(user.email(), user.password()));
     } catch(Exception e) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
