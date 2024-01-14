5bb970b06b3a17d574de2424f71c6f98199ba04d
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 19:43:04 -0300

    chore: added new method on repository

--- src/main/java/com/example/festivals/repository/UserRepository.java
+++ src/main/java/com/example/festivals/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.festivals.repository;
 
 import com.example.festivals.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
 
   Optional<User> findByEmailAndPassword(String email, String password);
+
+  UserDetails findByEmail(String email);
 
 }
