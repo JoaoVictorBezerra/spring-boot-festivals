68b6ec477a843bd207912eff17f5e24d3325ad1f
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 19:57:15 -0300

    feat: added SecurityConfig and change AuthorizationService

--- src/main/java/com/example/festivals/services/AuthorizationService.java
+++ src/main/java/com/example/festivals/services/AuthorizationService.java
@@ -3,21 +3,29 @@ package com.example.festivals.services;
 import com.example.festivals.dto.response.LoginResponseDTO;
 import com.example.festivals.entities.User;
 import com.example.festivals.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class LoginService {
+public class AuthorizationService implements UserDetailsService {
   private final UserRepository userRepository;
 
-  public LoginService(UserRepository userRepository) {
+  public AuthorizationService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-  public LoginResponseDTO execute(String email, String password) throws Exception {
+  public LoginResponseDTO login(String email, String password) throws Exception {
     Optional<User> userInfos = userRepository.findByEmailAndPassword(email, password);
     User user = userInfos.orElseThrow(() -> new Exception("Nenhum usuário encontrado"));
     return new LoginResponseDTO(user);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return this.userRepository.findByEmail(email);
   }
 }
