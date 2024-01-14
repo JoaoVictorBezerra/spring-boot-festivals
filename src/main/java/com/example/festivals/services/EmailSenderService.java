50bf950b1af49b92e23ab3f7899fbdbf2caf72e4
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 15:35:09 -0300

    refactor: refactor on AmazonSesService

--- src/main/java/com/example/festivals/services/EmailSenderService.java
+++ src/main/java/com/example/festivals/services/EmailSenderService.java
@@ -1,35 +1,7 @@
 package com.example.festivals.services;
-
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailSenderService {
-
-  private AmazonSimpleEmailService amazonSimpleEmailService;
-
-  @Autowired
-  public EmailSenderService(AmazonSimpleEmailService amazonSimpleEmailService) {
-    this.amazonSimpleEmailService = amazonSimpleEmailService;
-  }
-
-  public void sendEmail(String toEmail, String subject, String body) throws Exception {
-    SendEmailRequest request = new SendEmailRequest()
-        .withSource("dev.joaovictor@hotmail.com")
-        .withDestination(new Destination().withToAddresses(toEmail))
-        .withMessage(new Message()
-            .withSubject(new Content(subject))
-            .withBody(new Body().withText(new Content(body)))
-        );
-
-    try {
-      amazonSimpleEmailService.sendEmail(request);
-    } catch (AmazonServiceException ex) {
-      throw new Exception("Email sending failed");
-    }
-  }
-
+public interface EmailSenderService {
+  void sendEmail(String toEmail, String subject, String body) throws Exception;
 }
