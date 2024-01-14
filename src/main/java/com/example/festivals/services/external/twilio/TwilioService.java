7b210077154084d3a3f3eb49e9771be4ff4da69a
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 17:48:35 -0300

    refactor: apply Single Responsibility Principle on TwilioService

--- src/main/java/com/example/festivals/services/external/twilio/TwilioService.java
+++ src/main/java/com/example/festivals/services/external/twilio/TwilioService.java
@@ -3,6 +3,7 @@ package com.example.festivals.services.external.twilio;
 import com.example.festivals.services.SmsSender;
 import com.twilio.exception.TwilioException;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,18 @@ public class TwilioService implements SmsSender {
   @Override
   public void sendSms(String cellphone, String body) throws Exception {
     try {
-      Message message = Message
-          .creator(
-              new PhoneNumber(cellphone),
-              new PhoneNumber("+14062045953"),
-              body
-          )
-          .create();
+      buildEmailMessage(cellphone, body);
     } catch (TwilioException e) {
       throw new Exception(e);
     }
+  }
+
+  private static MessageCreator buildEmailMessage(String cellphone, String body) {
+    return Message
+        .creator(
+            new PhoneNumber(cellphone),
+            new PhoneNumber("+14062045953"),
+            body
+        );
   }
 }
