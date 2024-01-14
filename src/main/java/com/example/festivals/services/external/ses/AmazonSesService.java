d069cb6864a2ab2b292f0a69c1a38318fa5244cf
João Victor <dev.joaovictor@hotmail.com>
2024-01-14 17:48:22 -0300

    refactor: removing AutoWired notation from constructor of AmazonSesService and apply Single Responsibility Principle

--- src/main/java/com/example/festivals/services/external/ses/AmazonSesService.java
+++ src/main/java/com/example/festivals/services/external/ses/AmazonSesService.java
@@ -3,27 +3,28 @@ package com.example.festivals.services.external.ses;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.example.festivals.services.EmailSenderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AmazonSesService implements EmailSenderService {
   private final AmazonSimpleEmailService amazonSimpleEmailService;
 
-  @Autowired
   public AmazonSesService(AmazonSimpleEmailService amazonSimpleEmailService) {
     this.amazonSimpleEmailService = amazonSimpleEmailService;
   }
 
   @Override
   public void sendEmail(String toEmail, String subject, String body) {
-    SendEmailRequest request = new SendEmailRequest()
+    this.amazonSimpleEmailService.sendEmail(buildEseMessage(toEmail, subject, body));
+  }
+
+  private static SendEmailRequest buildEseMessage(String toEmail, String subject, String body) {
+    return new SendEmailRequest()
         .withSource("dev.joaovictor@hotmail.com")
         .withDestination(new Destination().withToAddresses(toEmail))
         .withMessage(new Message()
             .withSubject(new Content(subject))
             .withBody(new Body().withText(new Content(body)))
         );
-    this.amazonSimpleEmailService.sendEmail(request);
   }
 }
